67dd8311664a5d1d8269ef299f790b1cebd4ba62
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2016-05-03 09:57:16 +0800

    ios: swift_code: added basic codes and logics for Swift
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ ios/swift_codes/basic.swift
@@ -0,0 +1,36 @@
+// Swift comments
+
+// Indent (2-4 spaces)
+
+
+// String
+"This is a proper string"
+'Not a string'
+
+let message = "The value of the slider is: \(currentValue)"  // Joining String
+            + "\nThe target value is: \(targetValue)"
+
+// Boolean
+true
+false
+
+// Numbers
+2, 5, 123123, 5434534
+
+
+// Variables
+var variables = 5
+var targetValue: Int = 0 //Specify types
+
+// Constants
+let constants = 3.142
+
+
+// Number Conversion
+// Float to Long
+var long_number = lroundf(234.2342342)
+// Numbers to Float
+var float_number = Float(234234234)
+// Number to String
+var string = String(23423423)
+
---
+++ ios/swift_codes/logic.swift
@@ -0,0 +1,10 @@
+
+// If Else If
+var difference: Int
+if currentValue > targetValue {
+	difference = currentValue - targetValue
+} else if targetValue > currentValue {
+	difference = targetValue - currentValue
+} else {
+	difference = 0
+}
